7a2c0d089a78a38fe274e6daf2c31611f4871f9a
Heather Andres <heather.andres@dfo-mpo.gc.ca>
2024-02-13 14:11:15 -0400

    add precondition test on function inputs

--- tests/test_models.py
+++ tests/test_models.py
@@ -58,17 +58,21 @@ def test_daily_min_string():
         error_expected = daily_min([['Hello','there'],['General','Kenobi']])
 
 @pytest.mark.parametrize(
-    "test, expected",
+    "test, expected, expect_raises",
     [
-         ([[1,1,1],[1,1,1],[1,np.nan,2]],[[1,1,1],[1,1,1],[0.5,0,1]]),
-         ([[1,1,1],[0.0000001,10,2],[1,1,1]],[[1,1,1],[0,1,0.2],[1,1,1]]),
-         ([[99999999,1,4],[1,1,1],[1,1,1]], [[1,0,0],[1,1,1],[1,1,1]]),
-         ([[0,0,0], [0,0,0], [0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]),
-         ([[1,1,1], [1,1,1], [1,1,1]], [[1,1,1],[1,1,1],[1,1,1]]),
-         ([[1,2,3], [4,5,6], [7,8,9]], [[0.33,0.67,1],[0.67,0.83,1],[0.78,0.89,1]])
+         ([[1,1,1],[1,1,1],[1,np.nan,2]],[[1,1,1],[1,1,1],[0.5,0,1]],None),
+         ([[1,1,1],[0.0000001,10,2],[1,1,1]],[[1,1,1],[0,1,0.2],[1,1,1]],None),
+         ([[99999999,1,4],[1,1,1],[1,1,1]], [[1,0,0],[1,1,1],[1,1,1]],None),
+         ([[0,0,0], [0,0,0], [0,0,0]], [[0,0,0],[0,0,0],[0,0,0]],None),
+         ([[-1,1,1], [1,1,1], [1,1,1]], [[-1,1,1], [0,1,1], [1,1,1]], ValueError),
+         ([[1,2,3], [4,5,6], [7,8,9]], [[0.33,0.67,1],[0.67,0.83,1],[0.78,0.89,1]],None)
     ])
-def test_patient_normalise(test,expected):
+def test_patient_normalise(test,expected,expect_raises):
     """test normalisation works for arrays of one and positive integers.
     assumption that test accuracy of two decimal places is sufficient. """
     from inflammation.models import patient_normalise
-    npt.assert_almost_equal(patient_normalise(np.array(test)),np.array(expected),decimal=2)
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)),np.array(expected),decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)),np.array(expected),decimal=2)
