6b0a14c7bd3d167c27084df040580538f6f796b7
Heather Andres <heather.andres@dfo-mpo.gc.ca>
2024-02-15 15:50:19 -0400

    tidying up code using pylint

--- inflammation/models.py
+++ inflammation/models.py
@@ -10,7 +10,7 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -70,14 +70,13 @@ def patient_normalise(data):
         raise ValueError("Inflammation values should not be negative")
     if not isinstance(data,np.ndarray):
         raise TypeError("Inflammation input should be an ndarray")
-    else:
-        print(np.shape(data))
-        if len(np.shape(data))!=2:
-            raise TypeError("Inflammation input should two-dimensional")    
+    print(np.shape(data))
+    if len(np.shape(data))!=2:
+        raise TypeError("Inflammation input should two-dimensional")
 
-    max = np.nanmax(data,axis=1)
+    max_value = np.nanmax(data,axis=1)
     with np.errstate(invalid="ignore",divide="ignore"):
-        normalised = data/max[:,np.newaxis]
+        normalised = data/max_value[:,np.newaxis]
     normalised[np.isnan(normalised)] = 0
     normalised[normalised<0] = 0
     return normalised
